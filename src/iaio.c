a09aae9e0e86c13b0a10f457e9306eab1cec4ba4
jdegges <jdegges@defy.(none)>
2009-03-27 17:56:56 -0700

    fixed memory error when writing to SDL window

--- src/iaio.c
+++ src/iaio.c
@@ -57,7 +57,9 @@ static inline int iaio_displayimage( iaio_t* iaio, ia_image_t* iar )
     }
 
     for( i = 0; i < iaio->i_height; i++ )
-        ia_memcpy_pixel_to_uint8( (uint8_t*)iaio->screen->pixels+i*iaio->i_width*3, iar->pix+(iaio->i_height-i)*iaio->i_width*3, iaio->i_width*3 );
+        ia_memcpy_pixel_to_uint8( (uint8_t*)iaio->screen->pixels + iaio->i_width*3 * i,
+                                  iar->pix + iaio->i_width*3 * (iaio->i_height-i-1),
+                                  iaio->i_width*3 );
 
     if( SDL_MUSTLOCK(iaio->screen) ) {
         SDL_UnlockSurface( iaio->screen );
