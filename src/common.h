44ffa08ce2e2589c34a3913c667b6317b2be2f77
Joey Degges <jdegges@gmail.com>
2009-05-16 17:05:41 -0700

    added some functions to convert between ffmpeg and v4l2 pixel format names

--- src/common.h
+++ src/common.h
@@ -27,6 +27,7 @@
 #define _XOPEN_SOURCE 500
 #endif
 
+#include "config.h"
 #include <sys/select.h>
 #include <string.h>
 #include <stdio.h>
@@ -37,12 +38,16 @@
 #include <math.h>
 #include <pthread.h>
 
-#include "../config.h"
 #define ia_pixel_t uint8_t
 static const int debug = 0;
 
 #if HAVE_LIBAVCODEC && HAVE_LIBAVFORMAT && HAVE_LIBAVUTIL && HAVE_LIBSWSCALE && HAVE_LIBZ
 #define HAVE_FFMPEG
+#endif
+
+#ifdef HAVE_LINUX_VIDEODEV2_H
+#define HAVE_V4L2 1
+#include <linux/videodev2.h>
 #endif
 
 /* ia_image_t: image data structure
@@ -70,6 +75,129 @@ typedef struct ia_image_t
     pthread_cond_t cond_ro;
     pthread_cond_t cond_rw;
 } ia_image_t;
+
+typedef enum {
+    IA_PIX_FMT_NONE= -1,
+    IA_PIX_FMT_YUV420P,   ///< planar YUV 4:2:0, 12bpp, (1 Cr & Cb sample per 2x2 Y samples)
+    IA_PIX_FMT_YUYV422,   ///< packed YUV 4:2:2, 16bpp, Y0 Cb Y1 Cr
+    IA_PIX_FMT_RGB24,     ///< packed RGB 8:8:8, 24bpp, RGBRGB...
+    IA_PIX_FMT_BGR24,     ///< packed RGB 8:8:8, 24bpp, BGRBGR...
+    IA_PIX_FMT_YUV422P,   ///< planar YUV 4:2:2, 16bpp, (1 Cr & Cb sample per 2x1 Y samples)
+    IA_PIX_FMT_YUV444P,   ///< planar YUV 4:4:4, 24bpp, (1 Cr & Cb sample per 1x1 Y samples)
+    IA_PIX_FMT_RGB32,     ///< packed RGB 8:8:8, 32bpp, (msb)8A 8R 8G 8B(lsb), in CPU endianness
+    IA_PIX_FMT_YUV410P,   ///< planar YUV 4:1:0,  9bpp, (1 Cr & Cb sample per 4x4 Y samples)
+    IA_PIX_FMT_YUV411P,   ///< planar YUV 4:1:1, 12bpp, (1 Cr & Cb sample per 4x1 Y samples)
+    IA_PIX_FMT_RGB565,    ///< packed RGB 5:6:5, 16bpp, (msb)   5R 6G 5B(lsb), in CPU endianness
+    IA_PIX_FMT_RGB555,    ///< packed RGB 5:5:5, 16bpp, (msb)1A 5R 5G 5B(lsb), in CPU endianness, most significant bit to 0
+    IA_PIX_FMT_GRAY8,     ///<        Y        ,  8bpp
+    IA_PIX_FMT_MONOWHITE, ///<        Y        ,  1bpp, 0 is white, 1 is black
+    IA_PIX_FMT_MONOBLACK, ///<        Y        ,  1bpp, 0 is black, 1 is white
+    IA_PIX_FMT_PAL8,      ///< 8 bit with IA_PIX_FMT_RGB32 palette
+    IA_PIX_FMT_YUVJ420P,  ///< planar YUV 4:2:0, 12bpp, full scale (JPEG)
+    IA_PIX_FMT_YUVJ422P,  ///< planar YUV 4:2:2, 16bpp, full scale (JPEG)
+    IA_PIX_FMT_YUVJ444P,  ///< planar YUV 4:4:4, 24bpp, full scale (JPEG)
+    IA_PIX_FMT_XVMC_MPEG2_MC,///< XVideo Motion Acceleration via common packet passing
+    IA_PIX_FMT_XVMC_MPEG2_IDCT,
+    IA_PIX_FMT_UYVY422,   ///< packed YUV 4:2:2, 16bpp, Cb Y0 Cr Y1
+    IA_PIX_FMT_UYYVYY411, ///< packed YUV 4:1:1, 12bpp, Cb Y0 Y1 Cr Y2 Y3
+    IA_PIX_FMT_BGR32,     ///< packed RGB 8:8:8, 32bpp, (msb)8A 8B 8G 8R(lsb), in CPU endianness
+    IA_PIX_FMT_BGR565,    ///< packed RGB 5:6:5, 16bpp, (msb)   5B 6G 5R(lsb), in CPU endianness
+    IA_PIX_FMT_BGR555,    ///< packed RGB 5:5:5, 16bpp, (msb)1A 5B 5G 5R(lsb), in CPU endianness, most significant bit to 1
+    IA_PIX_FMT_BGR8,      ///< packed RGB 3:3:2,  8bpp, (msb)2B 3G 3R(lsb)
+    IA_PIX_FMT_BGR4,      ///< packed RGB 1:2:1,  4bpp, (msb)1B 2G 1R(lsb)
+    IA_PIX_FMT_BGR4_BYTE, ///< packed RGB 1:2:1,  8bpp, (msb)1B 2G 1R(lsb)
+    IA_PIX_FMT_RGB8,      ///< packed RGB 3:3:2,  8bpp, (msb)2R 3G 3B(lsb)
+    IA_PIX_FMT_RGB4,      ///< packed RGB 1:2:1,  4bpp, (msb)1R 2G 1B(lsb)
+    IA_PIX_FMT_RGB4_BYTE, ///< packed RGB 1:2:1,  8bpp, (msb)1R 2G 1B(lsb)
+    IA_PIX_FMT_NV12,      ///< planar YUV 4:2:0, 12bpp, 1 plane for Y and 1 for UV
+    IA_PIX_FMT_NV21,      ///< as above, but U and V bytes are swapped
+
+    IA_PIX_FMT_RGB32_1,   ///< packed RGB 8:8:8, 32bpp, (msb)8R 8G 8B 8A(lsb), in CPU endianness
+    IA_PIX_FMT_BGR32_1,   ///< packed RGB 8:8:8, 32bpp, (msb)8B 8G 8R 8A(lsb), in CPU endianness
+
+    IA_PIX_FMT_GRAY16BE,  ///<        Y        , 16bpp, big-endian
+    IA_PIX_FMT_GRAY16LE,  ///<        Y        , 16bpp, little-endian
+    IA_PIX_FMT_YUV440P,   ///< planar YUV 4:4:0 (1 Cr & Cb sample per 1x2 Y samples)
+    IA_PIX_FMT_YUVJ440P,  ///< planar YUV 4:4:0 full scale (JPEG)
+    IA_PIX_FMT_YUVA420P,  ///< planar YUV 4:2:0, 20bpp, (1 Cr & Cb sample per 2x2 Y & A samples)
+    IA_PIX_FMT_VDPAU_H264,///< H.264 HW decoding with VDPAU, data[0] contains a vdpau_render_state struct which contains the bitstream of the slices as well as various fields extracted from headers
+    IA_PIX_FMT_VDPAU_MPEG1,///< MPEG-1 HW decoding with VDPAU, data[0] contains a vdpau_render_state struct which contains the bitstream of the slices as well as various fields extracted from headers
+    IA_PIX_FMT_VDPAU_MPEG2,///< MPEG-2 HW decoding with VDPAU, data[0] contains a vdpau_render_state struct which contains the bitstream of the slices as well as various fields extracted from headers
+    IA_PIX_FMT_VDPAU_WMV3,///< WMV3 HW decoding with VDPAU, data[0] contains a vdpau_render_state struct which contains the bitstream of the slices as well as various fields extracted from headers
+    IA_PIX_FMT_VDPAU_VC1, ///< VC-1 HW decoding with VDPAU, data[0] contains a vdpau_render_state struct which contains the bitstream of the slices as well as various fields extracted from headers
+    IA_PIX_FMT_RGB48BE,   ///< packed RGB 16:16:16, 48bpp, 16R, 16G, 16B, big-endian
+    IA_PIX_FMT_RGB48LE,   ///< packed RGB 16:16:16, 48bpp, 16R, 16G, 16B, little-endian
+    IA_PIX_FMT_VAAPI_MOCO, ///< HW acceleration through VA API at motion compensation entry-point, Picture.data[3] contains a vaapi_render_state struct which contains macroblocks as well as various fields extracted from headers
+    IA_PIX_FMT_VAAPI_IDCT, ///< HW acceleration through VA API at IDCT entry-point, Picture.data[3] contains a vaapi_render_state struct which contains fields extracted from headers
+    IA_PIX_FMT_VAAPI_VLD,  ///< HW decoding through VA API, Picture.data[3] contains a vaapi_render_state struct which contains the bitstream of the slices as well as various fields extracted from headers
+    IA_PIX_FMT_NB,        ///< number of pixel formats, DO NOT USE THIS if you want to link with shared libav* because the number of formats might differ between versions
+} ia_pixel_format;
+
+typedef enum {
+    IA_PIX_FMT_FFMPEG,
+    IA_PIX_FMT_V4L2,
+} ia_pixel_format_type;
+
+static inline ia_pixel_format ia_convert_format( int pix_fmt, ia_pixel_format_type type )
+{
+#ifdef HAVE_FFMPEG
+    if( type == IA_PIX_FMT_FFMPEG ) {
+        return pix_fmt;
+    }
+#endif
+
+#ifdef HAVE_V4L2
+    if( type == IA_PIX_FMT_V4L2 ) {
+        switch( pix_fmt ) {
+            case V4L2_PIX_FMT_RGB332:   return IA_PIX_FMT_RGB32;
+            case V4L2_PIX_FMT_RGB444:   return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_RGB555:   return IA_PIX_FMT_RGB555;
+            case V4L2_PIX_FMT_RGB565:   return IA_PIX_FMT_RGB565;
+            case V4L2_PIX_FMT_RGB555X:  return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_RGB565X:  return IA_PIX_FMT_BGR565;
+            case V4L2_PIX_FMT_BGR24:    return IA_PIX_FMT_BGR24;
+            case V4L2_PIX_FMT_RGB24:    return IA_PIX_FMT_RGB24;
+            case V4L2_PIX_FMT_BGR32:    return IA_PIX_FMT_BGR32;
+            case V4L2_PIX_FMT_RGB32:    return IA_PIX_FMT_RGB32;
+            case V4L2_PIX_FMT_GREY:     return IA_PIX_FMT_GRAY8;
+            case V4L2_PIX_FMT_Y16:      return IA_PIX_FMT_GRAY16LE;
+            case V4L2_PIX_FMT_PAL8:     return IA_PIX_FMT_PAL8;
+            case V4L2_PIX_FMT_YVU410:   return IA_PIX_FMT_YUV410P;
+            case V4L2_PIX_FMT_YVU420:   return IA_PIX_FMT_YUV420P;
+            case V4L2_PIX_FMT_YUYV:     return IA_PIX_FMT_YUYV422;
+            case V4L2_PIX_FMT_UYVY:     return IA_PIX_FMT_UYVY422;
+            case V4L2_PIX_FMT_YUV422P:  return IA_PIX_FMT_YUV422P;
+            case V4L2_PIX_FMT_YUV411P:  return IA_PIX_FMT_YUV411P;
+            case V4L2_PIX_FMT_Y41P:     return IA_PIX_FMT_UYYVYY411;
+            case V4L2_PIX_FMT_YUV444:   return IA_PIX_FMT_YUV444P;
+            case V4L2_PIX_FMT_YUV555:   return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_YUV565:   return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_YUV32:    return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_NV12:     return IA_PIX_FMT_NV12;
+            case V4L2_PIX_FMT_NV21:     return IA_PIX_FMT_NV21;
+            case V4L2_PIX_FMT_YUV410:   return IA_PIX_FMT_YUV410P;
+            case V4L2_PIX_FMT_YUV420:   return IA_PIX_FMT_YUV420P;
+            case V4L2_PIX_FMT_YYUV:     return IA_PIX_FMT_UYVY422;
+            case V4L2_PIX_FMT_HI240:    return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_HM12:     return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_SBGGR8:   return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_SBGGR16:  return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_MJPEG:    return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_JPEG:     return IA_PIX_FMT_YUVJ444P;
+            case V4L2_PIX_FMT_DV:       return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_MPEG:     return IA_PIX_FMT_VDPAU_MPEG1;
+            case V4L2_PIX_FMT_WNVA:     return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_SN9C10X:  return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_PWC1:     return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_PWC2:     return IA_PIX_FMT_NONE;
+            case V4L2_PIX_FMT_ET61X251: return IA_PIX_FMT_NONE;
+            default:                    return IA_PIX_FMT_NONE;
+        }
+    }
+#endif
+
+    return IA_PIX_FMT_NONE;
+}
 
 static inline int ia_select( int n, fd_set* r, fd_set* w, fd_set* e, struct timeval* t )
 {
