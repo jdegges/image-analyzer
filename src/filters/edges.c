02c4eedbf6ea71fc24f1ed0a935b980c25c3ecbe
Joey Degges <jdegges@gmail.com>
2009-06-28 13:18:01 -0700

    updated the edge filter to use pitch properly. also refactored it a bit to make it easier to read

--- src/filters/edges.c
+++ src/filters/edges.c
@@ -22,53 +22,48 @@
 
 #include "edges.h"
 
+#define o(x,y,p) (pitch*(y)+(x)*3+p)
+
 void fstderiv_exec( ia_seq_t* s, ia_filter_param_t* fp, ia_image_t** iaim, ia_image_t* iar )
 {
     ia_image_t* iaf = iaim[0];
     //const double op = 255/sqrt(pow(255*3,2)*2); //  = max / (lmax - lmin)
-    int i;
+    int i, j;
+    int pitch = iar->i_pitch;
 
-    for( i = s->param->i_height; i--; )
+    for( i = 0; i < pitch*s->param->i_height; i++ ) {
+        iar->pix[i] = 0;
+    }
+
+    for( j = 0; j < s->param->i_height; j++ )
     {
-        int j;
-        for( j = s->param->i_width; j--; )
+        for( i = 1; i < s->param->i_width; i++ )
         {
             int ci, cj, pix;
-            double dx[3]; // {r, g, b}
-            double dy[3]; // {r, g, b}
+            double dx[3] = {0}; // {r, g, b}
+            double dy[3] = {0}; // {r, g, b}
 
-            if( i == 0 || j == 0 || i == s->param->i_height-1 || j == s->param->i_width-1 )
-            {
-                memset( &iar->pix[offset(s->param->i_width,j,i,0)], 0, sizeof(ia_pixel_t)*3 );
+            if( i == 0 || j == 0 || i == s->param->i_width-1 || j == s->param->i_height-1 )
                 continue;
-            }
 
-            for( pix = 3; pix--; ) {
-                dx[pix] =
-                dy[pix] = 0;
-            }
+            for( pix = 0; pix < 3; pix++ ) {
 
-            // left and right
-            for( ci = i-1; ci <= i+1; ci++ ) {
-                for( pix = 3; pix--; ) {
-                    dx[pix] -= iaf->pix[offset(s->param->i_width,j-1,ci,pix)]; // left
-                    dx[pix] += iaf->pix[offset(s->param->i_width,j+1,ci,pix)]; // right
+                // left and right
+                for( cj = j-1; cj < j+1; cj++ ) {
+                    dx[pix] -= iaf->pix[o(i-1,cj,pix)]; // left
+                    dx[pix] += iaf->pix[o(i+1,cj,pix)]; // right
                 }
-            }
 
-            // top and bottom
-            for( cj = j-1; cj <= j+1; cj++ ) {
-                for( pix = 3; pix--; ) {
-                    dy[pix] += iaf->pix[offset(s->param->i_width,cj,i-1,pix)];  // top
-                    dy[pix] -= iaf->pix[offset(s->param->i_width,cj,i+1,pix)];  // bottom
+                // top and bottom
+                for( ci = i-1; ci < i+1; ci++ ) {
+                    dy[pix] += iaf->pix[o(ci,j-1,pix)]; // top
+                    dy[pix] -= iaf->pix[o(ci,j+1,pix)]; // bottom
                 }
-            }
 
-            // to scale the resultant pixel down to its appropriate value it
-            // should be multiplied by op, but visually, it looks better to
-            // clip it... this may change in the future.
-            for( pix = 3; pix--; ) {
-                iar->pix[offset(s->param->i_width,j,i,pix)] = clip_uint8( sqrt(pow(dx[pix],2)+pow(dy[pix],2)) );
+                // to scale the resultant pixel down to its appropriate value it
+                // should be multiplied by op, but visually, it looks better to
+                // clip it... this may change in the future.
+                iar->pix[o(i,j,pix)] = clip_uint8( sqrt(pow(dx[pix],2)+pow(dy[pix],2)) );
             }
         }
     }
